6e9928b7f05d04907ab40b452f2c494ca1bbb021
Devaraj-G <d.gopinathan@ucl.ac.uk>
2026-05-12 22:59:21 +0100

    gitgignores envs folder for virtual environment, adds jupytext to setup, and initial notebook

---
+++ python/cl_introduction_to_deep_learning/setup.py
@@ -0,0 +1,29 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.2
+#   kernelspec:
+#     display_name: dl_workshop
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import sklearn
+print('sklearn version: ', sklearn.__version__)
+
+import seaborn
+print('seaborn version: ', seaborn.__version__)
+
+import pandas
+print('pandas version: ', pandas.__version__)
+
+import tensorflow
+print('Tensorflow version: ', tensorflow.__version__)
+
+# %%
--- python/cl_introduction_to_deep_learning/setup.sh
+++ python/cl_introduction_to_deep_learning/setup.sh
@@ -4,10 +4,12 @@
 mkdir -p cl_introduction_to_deep_learning
 mkdir -p cl_introduction_to_deep_learning/data 
 mkdir -p cl_introduction_to_deep_learning/documents
+mkdir -p cl_introduction_to_deep_learning/envs
 mkdir -p cl_introduction_to_deep_learning/outputs 
 mkdir -p cl_introduction_to_deep_learning/repos
 
-# Create files
+# Create filescd ..
+
 touch cl_introduction_to_deep_learning/README.md
 
 # Download repos
@@ -19,12 +21,12 @@ git clone https://github.com/UCSBCarpentry/ml-deeplearning-notebooks.git cl_intr
 curl -L -o cl_introduction_to_deep_learning/data/weather_prediction_dataset_light.csv https://zenodo.org/records/5071376/files/weather_prediction_dataset_light.csv
 curl -L -o cl_introduction_to_deep_learning/data/10970014.zip https://zenodo.org/api/records/10970014/files-archive && unzip cl_introduction_to_deep_learning/data/10970014.zip -d cl_introduction_to_deep_learning/data/10970014/
 
-
 # Create and activate a virtual environment called `dl_workshop`
-python3 -m venv cl_introduction_to_deep_learning/dl_workshop
-source cl_introduction_to_deep_learning/dl_workshop/bin/activate
+python3 -m venv cl_introduction_to_deep_learning/envs/dl_workshop
+source cl_introduction_to_deep_learning/envs/dl_workshop/bin/activate
 
 # Install the required packages in the environment
+python3 -m pip install ipykernel jupytext # only for vs-code
 python3 -m pip install jupyter seaborn scikit-learn pandas tensorflow pydot
 
 # (Optional) Install `graphviz`
